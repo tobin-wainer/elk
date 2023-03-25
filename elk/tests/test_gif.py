1cc4cb1a9e7ebd8c346933561f8094e241a76e02
Tom Wagg <twagg@college.harvard.edu>
2023-03-25 10:54:23 -0700

    better gif testing

--- elk/tests/test_gif.py
+++ elk/tests/test_gif.py
@@ -1,15 +1,17 @@
-import elk
 from elk.lightcurve import TESSCutLightcurve
 import lightkurve as lk
-import numpy as np
+from time import time
 
 search_results = lk.search_tesscut('NGC 7790')
 
 print(search_results[1])
 
-lc = TESSCutLightcurve(radius=2.9/60, lk_search_result=search_results[1], cutout_size=20,
-                       save_pixel_periodograms=True, progress_bar=True)
+lc = TESSCutLightcurve(radius=2.9/60, lk_search_result=search_results[1], cutout_size=40,
+                       save_pixel_periodograms=True, progress_bar=False)
 
 lc.correct_lc()
 
-lc.make_periodogram_peak_pixels_gif('.', freq_bins=np.logspace(-1, 1, 20))
+
+start = time()
+lc.make_periodogram_peak_pixels_gif('.', freq_bins='auto')
+print("Runtime:", time() - start)
