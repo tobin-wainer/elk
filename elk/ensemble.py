f34094ee943467cd42eac93760632f21342a510a
Tobin Wainer <tobinwainer@gmail.com>
2023-05-15 16:17:06 -0700

    adding else statement to

--- elk/ensemble.py
+++ elk/ensemble.py
@@ -429,8 +429,8 @@ class EnsembleLC:
                       'which_sectors_good': [[lc.sector for lc in self.lcs if lc is not None]],
                       'n_failed_download': [self.n_failed_download], 'n_bad_quality': [self.n_bad_quality],
                       'n_scatter_light': [len(self.scattered_light_sectors)],
-                      'scattered_light_sectors': [self.scattered_light_sectors if len(self.scattered_light_sectors) > 0 else 0],
-                      'lc_lens': [[len(lc.corrected_lc) for lc in self.lcs if lc is not None]]})
+                      'scattered_light_sectors': [self.scattered_light_sectors if len(self.scattered_light_sectors) > 0 else -99],
+                      'lc_lens': [[len(lc.corrected_lc) for lc in self.lcs if lc is not None else -99]]})
 
 
 def from_fits(filepath, existing_class=None, **kwargs):
